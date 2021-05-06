483d00b02b0d3e435478ccc1ed3d4e19c80c3f12
Taehui Kim <taehuikim0430@gmail.com>
2021-05-06 13:07:54 +0900

    Initial Commit

---
+++ NodeiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NodeiOS
+//
+//  Created by Sinchon on 2021/05/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
